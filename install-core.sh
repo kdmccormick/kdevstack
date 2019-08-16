650315f79253136b5194488e509fb800c559cd00
Kyle McCormick <kmccormick@edx.org>
2019-08-16 10:12:49 -0400

    Repo rename; add install-core.sh

---
+++ install-core.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+# Run this from the root of the directory in which
+# you want to install modstack
+
+DEFAULT_ROOT=https://raw.githubusercontent.com/kdmccormick/modstack/master
+ROOT=${MODSTACK_URL:-DEFAULT_ROOT}
+
+curl $(ROOT)/.env > .env
+echo >> .env
+echo "# Overrides from .override.env are below this comment"
+echo >> .env
+cat .override.env >> .env || true
+
+curl $(ROOT)/service-bases.yml > service-bases.yml
+curl $(ROOT)/docker-compose.core.yml > docker-compose.yml
+
+curl $(ROOT)/.modstack.gitignore > .modstack.gitignore
+git config core.excludesfile .modstack.gitignore
