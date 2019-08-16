c4cee3da2837b9ca4ffb036b9c53f9cefda860d1
Kyle McCormick <kmccormick@edx.org>
2019-08-16 11:13:59 -0400

    Make .modstack.gitignore just a copy-paste thing

--- install-core.sh
+++ install-core.sh
@@ -5,14 +5,11 @@
 DEFAULT_ROOT=https://raw.githubusercontent.com/kdmccormick/modstack/master
 ROOT=${MODSTACK_URL:-DEFAULT_ROOT}
 
-curl $(ROOT)/.env > .env
+curl $ROOT/.env > .env
 echo >> .env
 echo "# Overrides from .override.env are below this comment"
 echo >> .env
 cat .override.env >> .env || true
 
-curl $(ROOT)/service-bases.yml > service-bases.yml
-curl $(ROOT)/docker-compose.core.yml > docker-compose.yml
-
-curl $(ROOT)/.modstack.gitignore > .modstack.gitignore
-git config core.excludesfile .modstack.gitignore
+curl $ROOT/service-bases.yml > service-bases.yml
+curl $ROOT/docker-compose.core.yml > docker-compose.yml
