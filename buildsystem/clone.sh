9b578a86a72d63a52f37f175a7f3dfa54b6b9abf
Kyle McCormick <kmccormick@edx.org>
2019-08-17 21:49:22 -0400

    wip: clone.sh

--- buildsystem/clone.sh
+++ buildsystem/clone.sh
@@ -7,12 +7,25 @@ do_clone() {
 		if [[ "$line" = \#* ]] || [[ -z "$line" ]]; then
 			continue
 		fi
-		echo $line | \
-		xargs git clone --single-branch \
-	                    --config core.symlinks=true \
-	                    --depth=1 \
-	                    --branch
+		repo_url=$(cut -d' ' -f1 <<<"$line")
+		branch=$(cut -d' ' -f2 <<<"$line")
+		dest_folder_name=$(cut -d' ' -f3 <<<"$line")
+		dest_folder="repos/$dest_folder_name"
+
+		if [[ -d "$dest_folder/.git" ]]; then
+			here=$(pwd)
+			cd $dest_folder && git pull
+			cd $here
+		else
+			git clone --single-branch \
+			          --config core.symlinks=true \
+			          --depth=1 \
+			          --branch $branch \
+			          $repo_url \
+			          $dest_folder
+		fi
 	done
 }
 
+mkdir -p repos
 do_clone < repos.lst
